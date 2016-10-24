f7bf72d816ca8e8014ff03011197f91c3d326810
ishowshao <ishowshao@me.com>
2016-10-24 10:46:59 +0800

    vue

--- index.js
+++ index.js
@@ -13,8 +13,39 @@ const header = new Vue({
     },
     methods: {
         toggleMenu: function () {
-            console.log(this.showMenu);
             this.showMenu = !this.showMenu
+        },
+        showApp: function (appName) {
+            app.currentApp = appName;
+            this.toggleMenu();
+        }
+    }
+});
+
+const app = new Vue({
+    el: '#container',
+    data: {
+        currentApp: 'after-flop',
+        selectActive: false,
+        hands: ['?', '?'],
+        boards: ['?', '?', '?', '?', '?'],
+        selectCardText: ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'],
+        selectKindText: ['♠', '♥', '♣', '♦'],
+        currentSelection: {
+            card: '',
+            kind: ''
+        }
+    },
+    methods: {
+        showSelect: function (index, type) {
+            console.log('call', index, type);
+            this.selectActive = true;
+        },
+        selectCard: function (card) {
+            this.currentSelection.card = card;
+        },
+        selectKind: function (kind) {
+            this.currentSelection.kind = kind;
         }
     }
 });
