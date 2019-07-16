f5d5efda91e121e519c93aa4d85c193f406c40e0
ishowshao <ishowshao@ishowshaodeMacBook-Pro.local>
2019-07-16 13:46:37 +0800

    Update test.js

--- src/js/test.js
+++ src/js/test.js
@@ -1,12 +1,17 @@
-let classifier = require('./classifier');
-let util = require('./util');
+const Classifier = require('./classifier');
+const util = require('./util');
 
 console.time('speed');
-for (let i = 0; i < 170000; i++) {
+for (let i = 0; i < 10000000; i++) {
     let cards = util.generate(7);
-// console.log(util.air(cards));
-    cards.forEach(card => classifier.push(card));
-    classifier.reset();
-// console.log(classifier.pattern);
+    // console.log(util.air(cards));
+    const classifier = new Classifier(cards);
+    const result = classifier.classify();
+    if (result.pattern === 'StraightFlush' || result.pattern === 'RoyalStraightFlush') {
+        console.log(cards, util.air(cards));
+        console.log(result);
+
+    }
+    // StraightFlush RoyalStraightFlush
 }
 console.timeEnd('speed');
